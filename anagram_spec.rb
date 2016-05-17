8a9445a66868a5cc33af0c5c8935530e59dd8681
vitogit <vitogit@users.noreply.github.com>
2016-05-17 12:24:28 -0300

    Second failed(red) test

--- anagram_spec.rb
+++ anagram_spec.rb
@@ -5,4 +5,10 @@ describe "anagram" do
       expect(anagram 'a').to match_array(['a'])
     end
   end
+  
+  context "word with two letter" do
+    it "return the same and the anagram" do
+      expect(anagram 'ab').to match_array(['ab', 'ba'])
+    end
+  end  
 end

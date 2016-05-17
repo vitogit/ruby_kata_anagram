384bff7cddc375c251db1628581b81c9beddc9d4
vitogit <vitogit@users.noreply.github.com>
2016-05-17 12:38:56 -0300

    Second refactor test

--- anagram.rb
+++ anagram.rb
@@ -2,9 +2,13 @@ def anagram (word)
   return [word] if word.size==1
   ret = []
   word.chars.each_with_index do |letter, i|
-    rest_word = word.chars.to_a
-    rest_word.delete_at(i)
-    ret << letter+rest_word.join
+    ret << letter+rest_of_word(word, i)
   end
   ret  
+end
+
+def rest_of_word(word, i)
+  word_array = word.chars.to_a
+  word_array.delete_at(i)
+  word_array.join
 end

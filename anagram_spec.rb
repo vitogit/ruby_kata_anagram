591444bb68ac9af735e4b79927fe9c78d674e54d
vitogit <vitogit@users.noreply.github.com>
2016-05-17 12:39:49 -0300

    Third red test

--- anagram_spec.rb
+++ anagram_spec.rb
@@ -5,10 +5,17 @@ describe "anagram" do
       expect(anagram 'a').to match_array(['a'])
     end
   end
-  
+
   context "word with two letter" do
     it "return the same and the anagram" do
       expect(anagram 'ab').to match_array(['ab', 'ba'])
     end
-  end  
+  end
+
+  context "word with 3 letter" do
+    it "return anagrams" do
+      expect(anagram 'abc').to match_array(['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    end
+  end
+
 end

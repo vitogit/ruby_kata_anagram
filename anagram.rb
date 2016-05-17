766d83911557df1d887d4925a3ce0c2204a4ac96
vitogit <vitogit@users.noreply.github.com>
2016-05-17 12:27:05 -0300

    Second green test

--- anagram.rb
+++ anagram.rb
@@ -1,3 +1,10 @@
 def anagram (word)
-  [word] if word.size==1
+  return [word] if word.size==1
+  ret = []
+  word.chars.each_with_index do |letter, i|
+    rest_word = word.chars.to_a
+    rest_word.delete_at(i)
+    ret << letter+rest_word.join
+  end
+  ret  
 end
